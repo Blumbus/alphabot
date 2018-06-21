5777392f0da4487bb88a733598f7c93622481f77
Blumbus <blakeamiot@gmail.com>
2018-06-21 03:08:02 -0400

    ship 4

--- commands/ship.py
+++ commands/ship.py
@@ -7,13 +7,12 @@ def out(thing1, thing2):
     disp1 = thing1
     disp2 = thing2
     if isinstance(thing1, discord.User):
-        print('thing1!')
         name1 = thing1.id
         disp1 = thing1.display_name
     if isinstance(thing2, discord.User):
-        print('thing2!')
         name2 = thing2.id
         disp2 = thing2.display_name
-    random.seed = bin(name1) + bin(name2)
+    v = sum(ord(x) for x in name1) + sum(ord(x) for x in name2)
+    random.seed = v
     val = round(random.random())
     return '**' + str(disp1) + '** and **' + str(disp2) + '**... a ' + str(val) + '% ship rate!'
