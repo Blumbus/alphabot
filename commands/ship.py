304699cb27caef2cf303f1ff9a6925c6f87bbe2d
Blumbus <blakeamiot@gmail.com>
2018-06-21 02:52:23 -0400

    ship 2

--- commands/ship.py
+++ commands/ship.py
@@ -6,10 +6,10 @@ def out(thing1, thing2):
     name2 = thing2
     disp1 = thing1
     disp2 = thing2
-    if type(thing1) is discord.User:
+    if type(thing1) is discord.User or type(thing1) is discord.Member:
         name1 = thing1.id
         disp1 = thing1.display_name
-    if type(thing2) is discord.User:
+    if type(thing2) is discord.User or type(thing2) is discord.Member:
         name2 = thing2.id
         disp2 = thing2.display_name
     random.seed = bin(name1) + bin(name2)
